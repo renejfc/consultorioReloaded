667d971a94f61595f40c79aad6205201632de3f4
Laura Bassani <bassanilaurarita@gmail.com>
2020-11-18 18:39:19 +0100

    added database access

--- src/dbSession.php
+++ src/dbSession.php
@@ -21,15 +21,27 @@ class DbSession
 
   private function getConnection()
   {
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $database = "citas";
-    $charset = "utf-8";
-    $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
-    $pdo = new pdo("mysql:host={$host};dbname={$database};charset{$charset}", $user, $pass, $options);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (!getenv('CLEARDB_DATABASE_URL')) {
+      $host = "localhost";
+      $user = "root";
+      $pass = "root";
+      $database = "citas";
+      $charset = "utf-8";
+      $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+      $pdo = new pdo("mysql:host={$host};dbname={$database};charset{$charset}", $user, $pass, $options);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    return $pdo;
+      return $pdo;
+    } else {
+      $dbopts = parse_url(getenv('CLEARDB_DATABASE_URL'));
+      $host = $dbopts["host"];
+      $user = $dbopts["user"];
+      $pass = $dbopts["pass"];
+      $database = "citas";
+      $charset = "utf-8";
+      $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+      $pdo = new pdo("mysql:host={$host};dbname={$database};charset{$charset}", $user, $pass, $options);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
   }
 }
